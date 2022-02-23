d24712533e19febaa872218915d88bbee18fce35
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-02-23 23:58:17 +0000

    comments on types

--- index.js
+++ index.js
@@ -3,20 +3,32 @@ const { ApolloServer, gql } = require("apollo-server")
 /**
  * here we're defining how a query should look
  */
+/**
+ * the 'String' here is a Scalar type
+ * in general, we have 2 options: we can return something that
+ * is scalar or an object type
+ * Scalar types are -> String, Int, Float, Boolean
+ */
 const typeDefs = gql`
   type Query {
     hello: String
+    numberOfAnimals: Int
   }
 `
 /**
  * here we are resolving the query, by returning information
  * depending on how it's defined above
  */
+/**
+ * types are strict, we cannot return something else of what
+ * we have declared in the schema
+ */
 const resolvers = {
   Query: {
     hello: () => {
       return "World!"
     },
+    numberOfAnimals: () => 55,
   },
 }
 
