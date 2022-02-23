3aabae1740c2e1535bbea1578cfe3aeab8fd7d4f
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-02-23 23:49:17 +0000

    type definitions & resolvers

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const { ApolloServer } = require("apollo-server")
+const { ApolloServer, gql } = require("apollo-server")
 
-const server = new ApolloServer()
+/**
+ * here we're defining how a query should look
+ */
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+/**
+ * here we are resolving the query, by returning information
+ * depending on how it's defined above
+ */
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "World!"
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
 
 server.listen().then(({ url }) => {
   console.log("🚀 Server is ready at:", url)
