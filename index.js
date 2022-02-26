44fe8692ac0966f094c3008632ee0cea1a8a2dcc
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-02-26 01:38:20 +0000

    querying for products of a specific category / relating one category to many products

--- index.js
+++ index.js
@@ -71,7 +71,10 @@ const resolvers = {
     },
   },
   Category: {
-    products: (parents, arg, context) => {},
+    products: (parent, args, context) => {
+      const categoryId = parent.id
+      return products.filter((product) => product.categoryId === categoryId)
+    },
   },
 }
 
